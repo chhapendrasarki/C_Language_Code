fab319d8c46112a450913cb67b35af5f96b09ccc
chhapendrasarki <chhapendrasarki27@gmail.com>
2026-05-17 14:31:25 +0530

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int array[5]; // This is One o Dimation array. There are Five elements in an array. arrar of type. what is name of an array "arrayOne".
+
+int main (void)
+{
+    array[0] = 10;
+    array[1] = 50;
+    array[2] = 30;
+    array[3] = 60;
+    array[4] = 1;
+
+    printf("Array[0] : %d\t",array[0]);
+    printf("Array[1] : %d\t", array[1]);
+    printf("Array[2] : %d\t", array[2]);
+    printf("Array[3] : %d\t", array[3]);
+    printf("Array[4] : %d\t", array[4]);
+
+    return(0);
+}
