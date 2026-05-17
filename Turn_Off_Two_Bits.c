bbe3c76c0c6d78310feac8a0209ed2502a5376fe
chhapendrasarki <chhapendrasarki27@gmail.com>
2026-05-17 14:06:30 +0530

    Add files via upload

---
+++ Turn_Off_Two_Bits.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int numberOne;
+int numberTwo;
+int numberPosition;
+int numberResult;
+int numberOfBits;
+
+int main (void)
+{
+	printf("Enter the number One : ");
+	scanf("%d", &numberOne);
+
+	printf("Enter the Position Number : ");
+	scanf("%d", &numberPosition);
+
+	printf("Enter the number Of Bits : ");
+	scanf("%d", &numberOfBits);
+
+	numberTwo = ~0;
+
+	numberTwo = numberTwo < numberOfBits;
+
+	numberTwo = ~numberTwo;
+
+	numberTwo = numberTwo << (numberPosition - 2);
+
+	numberTwo = ~numberTwo;
+
+	numberResult = numberOne & numberTwo;
+
+	printf("Result : %d\n", numberResult);
+
+	return(0);
+}
