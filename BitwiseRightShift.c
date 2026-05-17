8a6f1a78f3acadc16afd218587eb2453a92ecd16
chhapendrasarki <chhapendrasarki27@gmail.com>
2026-05-17 14:03:47 +0530

    Add files via upload

---
+++ BitwiseRightShift.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int numberOne = 5;
+int numberTwo = 2;
+int numberThree;
+
+int main (void)
+{
+	numberThree = numberOne >> numberTwo;
+
+	printf("%d >> %d = %d\n", numberOne, numberTwo, numberThree);
+
+	return(0);
+}
