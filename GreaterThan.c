8863726867edb1df3b8f9908cf9354a882ab8bf7
chhapendrasarki <chhapendrasarki27@gmail.com>
2026-05-17 13:53:53 +0530

    Add files via upload

---
+++ GreaterThan.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int numberOne;
+int numberTwo;
+int numberResult;
+
+int main(void)
+{
+    printf("Enter Your Value : ");
+    scanf("%d", &numberOne);
+    
+    printf("Enter Your Value : ");
+    scanf("%d", &numberTwo);
+    
+    numberResult = numberOne > numberTwo;
+    
+    printf("%d > %d = %d\n", numberOne, numberTwo, numberResult);
+    
+    printf("")
+    
+    return(0);
+}
---
+++ LessThanOrEquall.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int numberOne;
+int numberTwo;
+int numberResult;
+
+int main(void)
+{
+				printf("Enter Your Value : ");
+				scanf("%d", &numberOne);
+				
+				printf("Enter Your Value : ");
+				scanf("%d", &numberTwo);
+				
+				numberResult = numberOne <= numberTwo;
+				
+				printf("%d <= %d = %d\n", numberOne, numberTwo, numberResult);
+				
+				return(0);
+}
