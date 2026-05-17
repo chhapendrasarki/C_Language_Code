445c9837ee47817f7f2461105b48d179f60ec696
chhapendrasarki <chhapendrasarki27@gmail.com>
2026-05-17 14:21:37 +0530

    Add files via upload

---
+++ doWhileDemo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int i;
+
+int main (void)
+{
+    //Code.
+    i = 1;
+    do
+    {
+        printf("Hello, World!!!\n");
+        ++i;
+    }while(i <= 10);
+
+    return(0);
+}
